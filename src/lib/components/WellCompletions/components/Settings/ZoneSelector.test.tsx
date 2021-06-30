609d037a1e7b3c8cfff3445aa1afde4d71cc493c
Sanghavi <Avanti.Sanghavi@emerson.com>
2021-06-30 10:48:31 +0530

    change zone selector test

--- src/lib/components/WellCompletions/components/Settings/ZoneSelector.test.tsx
+++ src/lib/components/WellCompletions/components/Settings/ZoneSelector.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor, within} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "jest-styled-components";
 import React from "react";
@@ -24,34 +24,23 @@ describe("test Zone Selector", () => {
             type: "ui/updateFilteredZones",
         });
     });
-
-    it("select 'zone1' check box to dispatch redux action and show only zone1", async () => {
+ 
+    it('de-select zone1', async () => {
         render(<ZoneSelector />, {
             wrapper: Wrapper,
-        });
-
-        userEvent.click(screen.getByRole("button", { name: /remove all/i }));
+        });  
         const zoneFilter = screen.getByText(/select zone\(s\)\.\.\./i);
         userEvent.click(zoneFilter);
-        const dropdown = screen.getByPlaceholderText("Search...");
-        await waitFor(() => expect(dropdown).toBeVisible());
-        userEvent.type(dropdown, "{down}");
-        userEvent.type(dropdown, "{enter}");
-
-        expect(testStore.dispatch).toHaveBeenCalledTimes(3);
-        expect(testStore.dispatch).toHaveBeenNthCalledWith(1, {
+        const autocomplete = await screen.findByPlaceholderText('Search...');
+        const view = screen.getByTitle(/zone1/i);within(view).getByText(/zone1/i);  
+        fireEvent.click(view);
+        expect(testStore.dispatch).toHaveBeenCalledTimes(2);
+        expect(testStore.dispatch).toHaveBeenNthCalledWith(1,{
             payload: [],
             type: "ui/updateFilteredZones",
         });
         expect(testStore.dispatch).toHaveBeenNthCalledWith(2, {
-            payload: [],
-            type: "ui/updateFilteredZones",
-        });
-        expect(testStore.dispatch).toHaveBeenNthCalledWith(3, {
             payload: [
-                "zone1.1",
-                "zone1.2",
-                "zone1.3",
                 "zone2.1",
                 "zone2.2",
                 "zone2.3",
@@ -66,5 +55,5 @@ describe("test Zone Selector", () => {
             ],
             type: "ui/updateFilteredZones",
         });
-    });
+      });
 });
