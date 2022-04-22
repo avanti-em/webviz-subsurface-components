3868d910d79b026dd0c17014df40318ec89b750f
Sanghavi <Avanti.Sanghavi@emerson.com>
2022-04-22 14:22:00 +0530

    auto pull request

---
+++ react/cypress/integration/default_well_log_viewer_test.ts
@@ -0,0 +1,51 @@
+describe("Well Logs component", () => {
+    beforeEach(() => {
+        cy.visit(
+            "http://localhost:6006/?path=/story/welllogviewer-demo-synclogviewer--default"
+        );
+        cy.getIframeBody().find(
+            "#root > div > div:nth-child(3) > svg[role='progressbar']",
+            {
+                timeout: 20000,
+            }
+        );
+        cy.get("body").then(($body) => {
+            if ($body.find("#root > div > div.css-1q7pov5 > nav").length > 0) {
+                cy.get("body").type("s");
+            }
+            if (
+                $body.find("#root > div > div.react-draggable.css-p5zfqk")
+                    .length > 0
+            ) {
+                cy.get("body").type("a");
+            }
+        });
+        cy.getIframeBody()
+            .find("#root > div > div:nth-child(3) > svg[role='progressbar']", {
+                timeout: 30000,
+            })
+            .should("not.exist");
+    });
+
+    it("Should display default component", () => {
+        cy.matchImageSnapshot();
+    });
+
+    it("Should hide color legends", () => {
+        cy.getIframeBody().find('[id="layers-selector-button"]').click();
+        cy.getIframeBody().find('[id="Wells-switch"]').click({ force: true });
+        cy.wait(2000);
+        cy.getIframeBody().find('[id="layers-selector-button"]').click();
+        cy.matchImageSnapshot();
+    });
+
+    it("Should hide faults", () => {
+        cy.getIframeBody().find('[id="layers-selector-button"]').click();
+        cy.getIframeBody()
+            .find('[id="Fault polygons-switch"]')
+            .click({ force: true });
+        cy.wait(2000);
+        cy.getIframeBody().find('[id="layers-selector-button"]').click();
+        cy.matchImageSnapshot();
+    });
+});
